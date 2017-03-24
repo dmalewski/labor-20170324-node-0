694ef5a7c4d1fdc5e671d6235fa51d105432aa06
Diana Malewski <talim08@hotmail.de>
2017-03-24 15:05:43 +0100

    neue html

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello Universe!');
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 8080;
+
+const server = http.createServer((req,res) => {
+    fs.readFile('./index.html',(error,contents) => {
+    res.end(contents);
+    });
+
+});
+
+server.listen(PORT,(err) => {
+    if(err) {
+        return console.error(err.message);
+
+    }
+
+   console.log(`Server is listening on port ${PORT}...`);
+});
